b8f217cb933d840d92315a4a73d93910a2d30a71
RobertGemmaJr <robert_gemma@brown.edu>
2023-08-16 15:31:53 -0400

    ref: Move guess length to constants file

--- Project 1 - Word Clone/src/components/GuessInput/GuessInput.js
+++ Project 1 - Word Clone/src/components/GuessInput/GuessInput.js
@@ -1,24 +1,28 @@
 import React, { useState } from "react";
+import { GUESS_LENGTH } from "../../constants";
 
 function GuessInput({ submitGuess }) {
   const [guess, setGuess] = useState("");
 
+  // Handle onChange event for form input
+  const handleChange = (e) => {
+    const value = e.target.value.toUpperCase();
+    if (value.length <= GUESS_LENGTH) setGuess(e.target.value.toUpperCase());
+  };
+
+  // Handle onSubmit event for form
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (guess.length !== GUESS_LENGTH) return; // Do nothing if invalid guess
+
+    submitGuess(guess);
+    setGuess("");
+  };
+
   return (
-    <form
-      className="guess-input-wrapper"
-      onSubmit={(e) => {
-        e.preventDefault();
-        submitGuess(guess);
-        setGuess("");
-      }}
-    >
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
       <label htmlFor="guess-input">Enter guess:</label>
-      <input
-        id="guess-input"
-        type="text"
-        value={guess}
-        onChange={(e) => setGuess(e.target.value.toUpperCase())}
-      />
+      <input id="guess-input" type="text" value={guess} onChange={handleChange} />
     </form>
   );
 }
