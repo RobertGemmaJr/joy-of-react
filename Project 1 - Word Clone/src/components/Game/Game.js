6e4b2734050336906823eaf53b55f6a5d14fd6c8
RobertGemmaJr <robert_gemma@brown.edu>
2023-08-16 15:49:44 -0400

    feat: Add guess list component

--- Project 1 - Word Clone/src/components/Game/Game.js
+++ Project 1 - Word Clone/src/components/Game/Game.js
@@ -3,7 +3,8 @@ import { useState } from "react";
 import { sample } from "../../utils";
 import { WORDS } from "../../data";
 
-import GuessInput from "../GuessInput/GuessInput";
+import GuessInput from "../GuessInput";
+import GuessList from "../GuessList";
 
 const ANSWER = sample(WORDS); // Pick a random word on every pageload.
 console.info({ ANSWER }); // Log solution for debugging
@@ -12,19 +13,16 @@ function Game() {
   const [guesses, setGuesses] = useState([]);
 
   const submitGuess = (guess) => {
-    console.log({ guess });
     // TODO: Can only submit unique guesses
     // TODO: Max number of guesses is 6
-    const nextGuesses = [...guesses, guess];
+    const nextGuesses = [...guesses, { guess, id: Math.random() }];
     console.log(nextGuesses);
     setGuesses(nextGuesses);
   };
 
   return (
     <div>
-      {guesses.map((g) => (
-        <p key={g}>{g}</p>
-      ))}
+      <GuessList guesses={guesses} />
       <GuessInput submitGuess={submitGuess} />
     </div>
   );
