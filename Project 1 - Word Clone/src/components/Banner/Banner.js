53c4a7118210cb009b3b8e3f099928f52e08643f
RobertGemmaJr <robert_gemma@brown.edu>
2023-08-17 16:39:35 -0400

    ref: Refactor Banner into two internal components

--- Project 1 - Word Clone/src/components/Banner/Banner.js
+++ Project 1 - Word Clone/src/components/Banner/Banner.js
@@ -1,27 +1,38 @@
 import React from "react";
 
+const BANNER_CLASS = "banner";
+
+// Banner displayed when user wins the game
+function WonBanner({ numGuesses }) {
+  const message = `${numGuesses} guess${numGuesses > 1 ? "es" : ""}`;
+  return (
+    <div className={`happy ${BANNER_CLASS}`}>
+      <p>
+        <strong>Congratulations!</strong> Got it in <strong>{message}</strong>.
+      </p>
+    </div>
+  );
+}
+
+// Banner displayed when user loses the game
+function LostBanner({ answer }) {
+  return (
+    <div className={`sad ${BANNER_CLASS}`}>
+      <p>
+        Sorry, the correct answer is <strong>{answer}</strong>.
+      </p>
+    </div>
+  );
+}
+
 function Banner({ status, numGuesses, answer }) {
-  const bannerClass = "banner";
   switch (status) {
+    case "WON":
+      return <WonBanner numGuesses={numGuesses} />;
+    case "LOST":
+      return <LostBanner answer={answer} />;
     case "PLAYING":
       return null;
-    case "WON":
-      const message = `${numGuesses} guess${numGuesses > 1 ? "es" : ""}`;
-      return (
-        <div className={`happy ${bannerClass}`}>
-          <p>
-            <strong>Congratulations!</strong> Got it in <strong>{message}</strong>.
-          </p>
-        </div>
-      );
-    case "LOST":
-      return (
-        <div className={`sad ${bannerClass}`}>
-          <p>
-            Sorry, the correct answer is <strong>{answer}</strong>.
-          </p>
-        </div>
-      );
     default:
       throw new Error("Invalid status code: " + status);
   }
